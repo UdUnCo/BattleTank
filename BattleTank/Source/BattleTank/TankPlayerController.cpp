cadcb2f601cd94f7dd9164c9f1bd692f4c003fbe
Heratexx <dennis.koob@hotmail.de>
2018-08-14 18:36:20 +0200

    BT 29

---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankPlayerController.h"
+
+//if virtual the children can override / extend the methode
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay(); //Call parent BeginPlay first
+	
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerControler not possesing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerControler possesing: %s"), *(ControlledTank->GetName()));
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+
+
+
